2b0a885209108f1bd2d916ba41b0f058a90dd157
Alvaro Cano <alvaro.cano@zevenet.com>
2022-08-16 16:14:39 +0200

    modify the block delimiters, clean example file and improve code
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>

--- cs.pl
+++ cs.pl
@@ -21,15 +21,24 @@ my @list = ();
 
 my $comment_char = "//";
 my $look_for_all = '_';
+my $parse_desc = '#';
+my $not_parse_init_block_reg = '[^>][^>]';
+my $parse_init_block = '>>>';
+my $parse_end_block = '<<<';
+
+sub printDirFile
+{
+	system("ls $CS_DIR");
+}
 
 sub printHelp
 {
 	say "Usage:";
 	say "  cs <tool> [search string]";
 	say "  ";
-	say "use '*' tool to look for in every file";
+	say "use '$look_for_all' tool to look for in every file";
 	say "try with the following tools:";
-	system("ls $CS_DIR");
+	printDirFile();
 	exit 1;
 }
 
@@ -44,20 +53,20 @@ sub filterCS
 
 	while (my $line = <$fh>)
 	{
-		if (!$block && $line =~ /^([^>][^>][^#]*)\s*#\s*(.+)/) {
+		if (!$block && $line =~ /^($not_parse_init_block_reg[^$parse_desc]*)\s*$parse_desc\s*(.+)/) {
 			my $l = $1;
 			my $d = $2;
 			if ($d =~ /$st/ || $l =~ /$st/) {
 				push @list, {line => $l, desc => $d, block => 0};
 			}
-		} elsif ($line =~ /^<</) {
+		} elsif ($line =~ /^$parse_end_block/) {
 			$block = 0;
 			if ($obj->{desc} =~ /$st/ || $obj->{line} =~ /$st/) {
 				push @list, $obj;
 			}
 		} elsif ($block) {
 			$obj->{line} .= $line;
-		} elsif ($line =~ /^\>\>\s*(.*)/) {
+		} elsif ($line =~ /^$parse_init_block\s*(.*)/) {
 			$obj = { line => "", desc => $1, block => 1};
 			$block = 1;
 		}
@@ -89,7 +98,7 @@ if ( $ARGV[2] ne "" ) {
 	printHelp();
 } elsif ( ! -f $file && $filename ne $look_for_all) {
 	print "The cheatsheet '$file' does not exist, try with:\n";
-	system("ls $CS_DIR");
+	printDirFile();
 	exit 1;
 }
 
