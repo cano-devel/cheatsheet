ff1434b7871ba14674dd7244d7aaef3cb52f91cb
Alvaro Cano <alvaro.cano@zevenet.com>
2022-08-16 16:04:44 +0200

    add an option for multifile match
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>

--- cs.pl
+++ cs.pl
@@ -20,12 +20,14 @@ my $CS_DIR="$BASE_DIR/cheatsheets";
 my @list = ();
 
 my $comment_char = "//";
+my $look_for_all = '_';
 
 sub printHelp
 {
 	say "Usage:";
 	say "  cs <tool> [search string]";
 	say "  ";
+	say "use '*' tool to look for in every file";
 	say "try with the following tools:";
 	system("ls $CS_DIR");
 	exit 1;
@@ -34,6 +36,7 @@ sub printHelp
 sub filterCS
 {
 	my $file = $_[0];
+	my $st = $_[1];
 
 	open(my $fh,'<', $file);
 	my $obj;
@@ -42,10 +45,16 @@ sub filterCS
 	while (my $line = <$fh>)
 	{
 		if (!$block && $line =~ /^([^>][^>][^#]*)\s*#\s*(.+)/) {
-			push @list, {line => $1, desc => $2, block => 0};
+			my $l = $1;
+			my $d = $2;
+			if ($d =~ /$st/ || $l =~ /$st/) {
+				push @list, {line => $l, desc => $d, block => 0};
+			}
 		} elsif ($line =~ /^<</) {
 			$block = 0;
-			push @list, $obj;
+			if ($obj->{desc} =~ /$st/ || $obj->{line} =~ /$st/) {
+				push @list, $obj;
+			}
 		} elsif ($block) {
 			$obj->{line} .= $line;
 		} elsif ($line =~ /^\>\>\s*(.*)/) {
@@ -57,36 +66,43 @@ sub filterCS
 	close $fh;
 }
 
-
 sub printCS
 {
-	my $st = shift @_;
-
 	foreach my $it (@list) {
-		if ($it->{desc} =~ /$st/ || $it->{line} =~ /$st/) {
-			say "$comment_char $it->{desc}";
-			say "$it->{line}";
-			say "";
-		}
+		say "$comment_char $it->{desc}";
+		say "$it->{line}";
+		say "";
 	}
 }
+
 
 # check input
 if ( $ARGV[0] eq "" ) {
 	printHelp();
 }
 
-my $file = "$CS_DIR/$ARGV[0]";
+my $filename = $ARGV[0];
+my $file = "$CS_DIR/$filename";
 my $st = $ARGV[1] // ".*";
 
 if ( $ARGV[2] ne "" ) {
 	printHelp();
-} elsif ( ! -f $file) {
+} elsif ( ! -f $file && $filename ne $look_for_all) {
 	print "The cheatsheet '$file' does not exist, try with:\n";
 	system("ls $CS_DIR");
 	exit 1;
 }
 
+
 # main
-filterCS($file);
-printCS($st);
+if ($filename eq $look_for_all) {
+	opendir my $dh, $CS_DIR;
+	while ( my $f = readdir ($dh)) {
+		filterCS("$CS_DIR/$f", $st);
+	}
+	close $dh;
+} else {
+	filterCS($file, $st);
+}
+
+printCS();
